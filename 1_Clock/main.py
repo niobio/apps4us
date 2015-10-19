b422b3d0607a5ba435e920087f65b549fdd0dfb9
niobio <jtouron@virolai.com>
2015-10-19 20:43:02 +0200

    Commit inicial
     Cambios para hacer commit:
    	new file:   0_HelloWorld/.ipynb_checkpoints/0_HelloWorld-checkpoint.ipynb
    	new file:   0_HelloWorld/0_HelloWorld.ipynb
    	new file:   0_HelloWorld/HelloWorld.kv~
    	new file:   0_HelloWorld/hello.py
    	new file:   0_HelloWorld/hello.py~
    	new file:   0_HelloWorld/hello2.kv
    	new file:   0_HelloWorld/hello2.kv~
    	new file:   0_HelloWorld/hello2.py
    	new file:   0_HelloWorld/hello2.py~
    	new file:   0_HelloWorld/main.py~
    	new file:   1_Clock/.ipynb_checkpoints/1_Clock-checkpoint.ipynb
    	new file:   1_Clock/1_Clock.ipynb
    	new file:   1_Clock/clock.kv
    	new file:   1_Clock/clock.kv~
    	new file:   1_Clock/layout.png
    	new file:   1_Clock/main.py
    	new file:   1_Clock/main.py~

---
+++ 1_Clock/main.py
@@ -0,0 +1,17 @@
+# File: main.py
+from kivy.app import App
+from kivy.clock import Clock
+
+from time import strftime
+
+class ClockApp(App):
+
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime('%H:%M:%S')
+    
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
+
+    
+if __name__ == '__main__':
+    ClockApp().run()
