c7926c456709acff07511d2958936c40a27b7c46
niobio <jtouron@virolai.com>
2015-12-14 23:14:02 +0100

    Cambios para hacer commit: 	deleted:    02_Widgets_basicos/code/mireia/suerte.kv 	deleted:    02_Widgets_basicos/code/mireia/suerte.kv~ 	deleted:    02_Widgets_basicos/code/mireia/suerte.py 	deleted:    02_Widgets_basicos/code/mireia/suerte.py~ 	new file:   03_Layouts/.ipynb_checkpoints/Layouts-checkpoint.ipynb 	new file:   03_Layouts/Layouts.ipynb 	new file:   03_Layouts/code/floatlayout.kv 	new file:   03_Layouts/code/floatlayout.py 	new file:   03_Layouts/img/floatlayout.png

---
+++ 03_Layouts/code/floatlayout.py
@@ -0,0 +1,10 @@
+# Archivo: floatlayout.py
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+class FloatLayoutApp(App):
+    def build(self):
+        return FloatLayout()
+
+if __name__=="__main__":
+    FloatLayoutApp().run()
